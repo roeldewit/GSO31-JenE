522b25ca98cb726196160ad5c745fd807cc11f1a
Astrid <Astrid@Probook-450-G2>
2016-01-10 20:15:28 +0100

    Package toegevoegd met SuiteClasses en TestRunner

---
+++ BankierenNoObserverFX/test/bank/test/SuiteBankieren.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bank.test;
+
+import bank.bankieren.BalieTest;
+import bank.bankieren.BankTest;
+import bank.bankieren.SessieTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * The JUnit test suite that contains tests from the test package bank.bankieren.
+ *
+ * @author Astrid
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ BalieTest.class, BankTest.class, SessieTest.class })
+public class SuiteBankieren {
+    
+}
